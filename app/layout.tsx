7286a967ba54831d607cd47c24b1924a9c7fc569
limitlessnx <newtinoski@gmail.com>
2026-08-02 16:36:21 -0400

    Load global action overlay styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css';
+import './action-loader.css';
 import type { Metadata } from 'next';
 import { Suspense } from 'react';
 import { GlobalActionLoader } from '@/components/GlobalActionLoader';
